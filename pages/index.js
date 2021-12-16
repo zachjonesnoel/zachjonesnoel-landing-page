7dbfd8729b3e5680d8d05d7b5b7ea4d90d1910dc
Jones Zachariah Noel N <joneszachariahnoel@gmail.com>
2021-12-16 21:31:34 +0530

    added RUM

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,13 @@ export default function Home() {
       <Head>
         <title>zachjonesnoel</title>
         <link rel="icon" href="/favicon.ico" />
-        <script type="text/javascript" src="/public/rum.js"></script>
+        {/* <script type="text/javascript" src="/public/rum.js"></script> */}
+        <script
+                    type='text/javascript'
+                    dangerouslySetInnerHTML={{ __html: `
+                    (function(n,i,v,r,s,c,x,z){x=window.AwsRumClient={q:[],n:n,i:i,v:v,r:r,c:c};window[n]=function(c,p){x.q.push({c:c,p:p});};z=document.createElement('script');z.async=true;z.src=s;document.head.insertBefore(z,document.getElementsByTagName('script')[0]);})('cwr','55b0ca8b-d756-4c7a-9880-e1a7a0232da7','1.0.0','us-east-1','https://client.rum.us-east-1.amazonaws.com/1.0.2/cwr.js',{sessionSampleRate:1,guestRoleArn:"arn:aws:iam::228628157461:role/RUM-Monitor-us-east-1-228628157461-6340433669361-Unauth",identityPoolId:"us-east-1:1068a686-9ff4-4c47-b2ad-577d860ceb9d",endpoint:"https://dataplane.rum.us-east-1.amazonaws.com",telemetries:["performance","errors","http"],allowCookies:true,enableXRay:false});
+                  ` }}
+                />
       </Head>
       <AnimatedBackground/>
       <main className="max-w-4xl mx-auto mt-16 antialiased">
