88507a441eca647bbb6e93ce6717cf5ec2138ee1
zachjonesnoel <joneszachariah.noeln@freshworks.com>
2023-02-05 17:22:30 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'standalone',
+  swcMinify: true,
 }
 
 module.exports = nextConfig
